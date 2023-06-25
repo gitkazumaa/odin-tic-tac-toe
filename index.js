5c6e20f1d1101f232ad9854e78f27cfc4b3b8bed
gitkazumaa <kazumaalternate@gmail.com>
2023-06-25 11:52:55 -0700

    Add is win function

--- index.js
+++ index.js
@@ -1,16 +1,66 @@
 const playerController = (() => {
     let playersArray = [];
     let playerTurn = 0;
+
     const setPlayerTurn = () => playerTurn % 2 === 0 ? playerTurn = 0 : playerTurn = 1;
+
     const markBoard = (tile, r, c) => {
         if (!gameboard.board[r][c]) {
             let mark = playersArray[playerTurn].playerMarker;
             gameboard.board[r][c] = mark;
             tile.innerHTML = mark;
+            console.log(isWin(mark, r, c));
             playerTurn++;
             setPlayerTurn();
         }
     }
+    const isWin = (mark, r, c) => {
+        let currentLength = 0;
+        for (let col = 0; col < 3; col++) {
+            if (gameboard.board[r][col] === mark) {
+                currentLength++
+            } else {
+                break;
+            }
+        }
+        if (currentLength === 3) return true;
+
+        currentLength = 0;
+        for (let row = 0; row < 3; row++) {
+            if (gameboard.board[row][c] === mark) {
+                currentLength++
+            } else {
+                break;
+            }
+        }
+        if (currentLength === 3) return true;
+
+        currentLength = 0;
+        let col = 0;
+        for (let row = 0; row < 3; row++) {
+            if (gameboard.board[row][col] === mark) {
+                currentLength++
+                col++;
+            } else {
+                break;
+            }
+        }
+        if (currentLength === 3) return true;
+
+        currentLength = 0;
+        col = 2;
+        for (let row = 0; row < 3; row++) {
+            if (gameboard.board[row][col] === mark) {
+                currentLength++
+                col--;
+            } else {
+                break;
+            }
+        }
+        if (currentLength === 3) return true;
+        return false;
+    }
+
     return {playersArray, markBoard};
 })();
 
@@ -19,8 +69,10 @@ const gameboard = (() => {
                    ["", "", ""], 
                    ["", "", ""]
                 ];
+
     const gameBoardElement = document.createElement("div");
-    gameBoardElement.setAttribute("class", "game-board")
+    gameBoardElement.setAttribute("class", "game-board");
+
     const displayBoard = () => {
         for (let r = 0; r < 3; r ++) {
             for (let c = 0; c < 3; c++) {
@@ -33,12 +85,14 @@ const gameboard = (() => {
         }
         document.body.appendChild(gameBoardElement);
     };
+
     return {board, displayBoard};
 })();
 
 const player = (name) => {
     const playerName = name;
     const playerMarker = playerController.playersArray.length === 0 ? "X" : "O";
+
     return {playerName, playerMarker};
 }
 
